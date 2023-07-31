a9e25a663d2eb75d38410ec5066d248d38d65470
hyeri6318 <lhl6318@naver.com>
2023-08-01 00:18:42 +0900

    solved.ac_10869번

---
+++ Essential01/p_10869.py
@@ -0,0 +1,12 @@
+"""
+입력 : 자연수 A,B
+출력 : 첫째 줄 A+B, 둘째 줄 A-B, 셋째 줄 A*B, 넷째 줄 A/B, 다섯째 줄 A%B
+"""
+
+A, B = map(int, input().split())
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
