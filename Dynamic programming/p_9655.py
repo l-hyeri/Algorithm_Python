7122a08e9e34b99c84cceecbc6507ed9a52682aa
hyeri6318 <lhl6318@naver.com>
2023-08-07 15:58:46 +0900

    solved.ac_9655번(DP)_수정

--- Dynamic programming/p_9655.py
+++ Dynamic programming/p_9655.py
@@ -3,15 +3,15 @@
 출력 : 상근이가 이김 - SK 출력, 창영이가 이김 - CY 출력
 """
 
-# 돌 개수 입력받기
 N = int(input())
 
-# 리스트 초기화
-dp = [0 for i in range(N + 1)]
+dp = [0] * 1001  # 미리 trun횟수를 저장하기 위한 리스트 (N이 1~1000이기때문에 1001번까지 리스트 생성)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 1
 
-for i in range(1, N + 1):
-    dp[i] = int(i / 3) + int(i % 3)
-    # print(i, dp[i])
+for i in range(4, N + 1):
+    dp[i] = min(dp[i - 1], dp[i - 3]) + 1
 
 if dp[N] % 2 == 1:
     print('SK')
