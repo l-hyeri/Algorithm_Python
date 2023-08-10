c7065385f2f26eb7e16864d5e6f7f4e7926c1187
hyeri6318 <lhl6318@naver.com>
2023-08-10 21:51:57 +0900

    solved.ac_1463번(DP)

---
+++ Dynamic programming/p_1463.py
@@ -0,0 +1,18 @@
+"""
+입력 : 정수 X
+출력 : 연산을 하는 횟수의 최솟값 출력
+"""
+
+n = int(input())
+
+dp = [0] * (n+1)
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
+
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+
+print(dp[n])
